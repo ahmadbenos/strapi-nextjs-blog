7d2cfc173324d560f0a35146fdf9be114b12179b
Ahmad <benos.tarabain@gmail.com>
2021-03-09 01:05:41 +0200

    progress

---
+++ frontend/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-center mt-2">Ahmad's Blog</h1>
+      <p className="text-center text-muted">
+        Strapi(headless CMS), NextJS, and MongoDB project
+      </p>
+      <p className="text-center">
+        <a href="https://github.com/ahmadbenos/strapi-nextjs-blog">
+          By Ahmad Tarabein
+          <span className="text-dark">
+            <i class="fab fa-github"></i>
+          </span>
+        </a>
+      </p>
+    </>
+  );
+};
+
+export default Header;
---
+++ frontend/pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+            integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+            crossorigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
